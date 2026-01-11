10603ad612266e561f68beba7d7d4e1bb07c8dd5
Vladimir Pavlov <v.pavlovv006@gmail.com>
2026-01-12 00:39:32 +0200

    Defining most functions
    Adding the needed functions for the game loop and also for random number generator.

--- Student-City-Simulator/Student-City-Simulator.cpp
+++ Student-City-Simulator/Student-City-Simulator.cpp
@@ -1,9 +1,11 @@
 ﻿#include <iostream>
 #include <string>
-#include <windows.h>
+#include <ctime>
+
 using namespace std;
 
-int SESSION = 45;
+int SESSION_DAYS = 45;
+int MAX = 100;
 
 struct Student
 {
@@ -34,13 +36,53 @@ void showStats() {
     cout << "======================= " << endl;
 }
 
-// Зареждане на игра от файл
-void loadGameLoop() {
+int randomInt(int min, int max) {
+    int random = rand() % (max - min + 1) + min;
+
+    return random;
+}
+
+void eat() {
 
 }
 
-// Започване на нова игра
-void newGameLoop() {
+void learn() {
+
+}
+
+void sleep() {
+
+}
+
+void goOut() {
+
+}
+
+void work() {
+
+}
+
+void attendExam() {
+
+}
+
+void exitGame() {
+
+}
+
+void autoSave() {
+
+}
+
+void win() {
+
+}
+
+void lose() {
+
+}
+
+void setDifficulty() {
     int difficulty;
 
     cout << "Choose difficulty:" << endl;
@@ -50,77 +92,110 @@ void newGameLoop() {
     cin >> difficulty;
 
     switch (difficulty) {
-        case 1:
-            student.Z = 70;
-            student.P = 100;
-            student.E = 100;
-            student.K = 100;
-            student.I = 0;
-            break;
-        case 2:
-            student.Z = 50;
-            student.P = 80;
-            student.E = 80;
-            student.K = 80;
-            student.I = 0;
-            break;
-        case 3:
-            student.Z = 35;
-            student.P = 40;
-            student.E = 60;
-            student.K = 60;
-            student.I = 0;
-            break;
-        default :
-            cout << "Invalid input!";
-            return;
+    case 1:
+        student.Z = 70;
+        student.P = 100;
+        student.E = 100;
+        student.K = 100;
+        student.I = 0;
+        break;
+    case 2:
+        student.Z = 50;
+        student.P = 80;
+        student.E = 80;
+        student.K = 80;
+        student.I = 0;
+        break;
+    case 3:
+        student.Z = 35;
+        student.P = 40;
+        student.E = 60;
+        student.K = 60;
+        student.I = 0;
+        break;
+    default:
+        cout << "Invalid input!";
+        return;
     }
+}
+
+// Зареждане на игра от файл
+void loadGameLoop() {
+
+}
+
+// Започване на нова игра
+void newGameLoop() {
+    setDifficulty();
 
     int day = 1;
 
-    while (day <= SESSION) {
+    while (day <= SESSION_DAYS) {
         showStats();
 
-        int option;
-        cout << "What do you feel like doing today?" << endl;
-        cout << "[1]Learn" << endl;
-        cout << "[2]Eat(Duner mostly) " << endl;
-        cout << "[3]Go out(chalgoteka preferably)" << endl;
-        cout << "[4]Rest in peace" << endl;
-        cout << "[5]Work" << endl;
-        cout << "[6]Attend exam(when it is time)" << endl;
-        cout << "[.]" << endl;
-        cout << "[.]" << endl;
-        cout << "[.]" << endl;
-        cout << "[11]Exit game" << endl;
-        cin >> option;
+        int isDecided = false;
 
-        switch (option) {
+        while (!isDecided)
+        {
+            int option;
+            cout << "What do you feel like doing today?" << endl;
+            cout << "[1]Learn" << endl;
+            cout << "[2]Eat(Duner mostly) " << endl;
+            cout << "[3]Go out(chalgoteka preferably)" << endl;
+            cout << "[4]Rest in peace" << endl;
+            cout << "[5]Work" << endl;
+            cout << "[6]Attend exam(when it is time)" << endl;
+            cout << "[.]" << endl;
+            cout << "[.]" << endl;
+            cout << "[.]" << endl;
+            cout << "[11]Exit game" << endl;
+            cin >> option;
+
+            switch (option) {
             case 1:
+                learn();
+                isDecided = true;
                 break;
             case 2:
+                eat();
+                isDecided = true;
                 break;
             case 3:
+                goOut();
+                isDecided = true;
                 break;
             case 4:
+                sleep();
+                isDecided = true;
                 break;
             case 5:
+                work();
+                isDecided = true;
                 break;
             case 6:
+                attendExam();
+                isDecided = true;
                 break;
             case 11:
+                exitGame();
+                isDecided = true;
                 break;
             default:
                 break;
+            }
         }
 
         day++;
+
+        autoSave();
     }
 
 }
 
 int main()
 {
+    srand(time(NULL));
+
     int option;
 
     cout << "Welcome to Student City!" << endl;
