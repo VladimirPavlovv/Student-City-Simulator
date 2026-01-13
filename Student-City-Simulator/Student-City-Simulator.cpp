14b19d262be9c7389ebe484b5199bb5c3edf05a4
Vladimir Pavlov <v.pavlovv006@gmail.com>
2026-01-14 01:06:12 +0200

    Initializing game loop functions
    Adding core game functions for learning, eating, sleeping, working, going out and attending exam.

--- Student-City-Simulator/Student-City-Simulator.cpp
+++ Student-City-Simulator/Student-City-Simulator.cpp
@@ -4,8 +4,8 @@
 
 using namespace std;
 
-int SESSION_DAYS = 45;
-int MAX = 100;
+const int SESSION_DAYS = 45;
+const int MAX = 100;
 
 struct Student
 {
@@ -42,27 +42,311 @@ int randomInt(int min, int max) {
     return random;
 }
 
-void eat() {
+void learn() {
+    int option;
 
+    cout << "How would you like to learn?" << endl;
+    cout << "[1]Go to lectures" << endl;
+    cout << "[2]Learn at home" << endl;
+    cout << "[3]Learn with friends" << endl;
+    cin >> option;
+
+    bool isDecided = false;
+
+    while (!isDecided) {
+        switch (option) {
+        case 1:
+            student.E -= 10;
+            student.P -= 10;
+
+            if (student.E >= 80) {
+                student.Z += 20;
+
+                if (student.Z > MAX) student.Z = MAX;
+            }
+            else if (student.E <= 40) {
+                int random = randomInt(1, 2);
+
+                if (random == 1) {
+                    student.Z += 20;
+
+                    if (student.Z > MAX) student.Z = MAX;
+                }
+                else {
+                    student.Z += 10;
+
+                    if (student.Z > MAX) student.Z = MAX;
+                }
+            }
+            else {
+                int random = randomInt(1, 4);
+
+                if (random != 1) {
+                    student.Z += 20;
+
+                    if (student.Z > MAX) student.Z = MAX;
+                }
+                else {
+                    student.Z += 10;
+
+                    if (student.Z > MAX) student.Z = MAX;
+                }
+            }
+            isDecided = true;
+            break;
+        case 2:
+            student.E -= 15;
+            student.P -= 20;
+
+            if (student.E >= 80) {
+                student.Z += 15;
+
+                if (student.Z > MAX) student.Z = MAX;
+            }
+            else if (student.E <= 40) {
+                int random = randomInt(1, 2);
+
+                if (random == 1) {
+                    student.Z += 15;
+
+                    if (student.Z > MAX) student.Z = MAX;
+                }
+                else {
+                    student.Z += 8;
+
+                    if (student.Z > MAX) student.Z = MAX;
+                }
+            }
+            else {
+                int random = randomInt(1, 4);
+
+                if (random != 1) {
+                    student.Z += 15;
+
+                    if (student.Z > MAX) student.Z = MAX;
+                }
+                else {
+                    student.Z += 8;
+
+                    if (student.Z > MAX) student.Z = MAX;
+                }
+            }
+            isDecided = true;
+            break;
+        case 3:
+            student.E -= 10;
+
+            if (student.E >= 80) {
+                student.Z += 5;
+                student.P += 10;
+
+                if (student.Z > MAX) student.Z = MAX;
+                if (student.P > MAX) student.P = MAX;
+            }
+            else if (student.E <= 40) {
+                int random = randomInt(1, 2);
+
+                if (random == 1) {
+                    student.Z += 5;
+                    student.P += 10;
+
+                    if (student.Z > MAX) student.Z = MAX;
+                    if (student.P > MAX) student.P = MAX;
+                }
+                else {
+                    student.P += 5;
+
+                    if (student.P > MAX) student.P = MAX;
+                }
+            }
+            else {
+                int random = randomInt(1, 4);
+
+                if (random != 1) {
+                    student.Z += 5;
+                    student.P += 10;
+
+                    if (student.Z > MAX) student.Z = MAX;
+                    if (student.P > MAX) student.P = MAX;
+                }
+                else {
+                    student.P += 5;
+
+                    if (student.P > MAX) student.P = MAX;
+                }
+            }
+            isDecided = true;
+            break;
+        default:
+            cout << "Invalid option!" << endl;
+            break;
+        }
+    }
+
+    //add chance for random effect
 }
 
-void learn() {
+void eat() {
+    student.K -= 10;
 
+    if (student.E >= 80) {
+        student.E += 20;
+        student.P += 5;
+
+        if (student.E > MAX) student.E = MAX;
+        if (student.P > MAX) student.P = MAX;
+    }
+    else if (student.E <= 40) {
+        int random = randomInt(1, 2);
+
+        if (random == 1) {
+            student.E += 20;
+            student.P += 5;
+
+            if (student.E > MAX) student.E = MAX;
+            if (student.P > MAX) student.P = MAX;
+        }
+        else {
+            student.E += 10;
+
+            if (student.E > MAX) student.E = MAX;
+        }
+    }
+    else {
+        int random = randomInt(1, 4);
+
+        if (random != 1) {
+            student.E += 20;
+            student.P += 5;
+
+            if (student.E > MAX) student.E = MAX;
+            if (student.P > MAX) student.P = MAX;
+        }
+        else {
+            student.E += 10;
+
+            if (student.E > MAX) student.E = MAX;
+        }
+    }
+
+    //add chance for random effect
 }
 
 void sleep() {
+    student.E += 50;
+    student.P += 10;
 
+    if (student.E > MAX) student.E = MAX;
+    if (student.P > MAX) student.P = MAX;
+
+    //add chance for random effect
 }
 
 void goOut() {
+    student.E -= 15;
+    student.K -= 25;
 
+    if (student.E >= 80) {
+        student.P += 40;
+
+        if (student.P > MAX) student.P = MAX;
+    }
+    else if (student.E <= 40) {
+        int random = randomInt(1, 2);
+
+        if (random == 1) {
+            student.P += 40;
+
+            if (student.P > MAX) student.P = MAX;
+        }
+        else {
+            student.P += 20;
+
+            if (student.P > MAX) student.P = MAX;
+        }
+    }
+    else {
+        int random = randomInt(1, 4);
+
+        if (random != 1) {
+            student.P += 40;
+
+            if (student.P > MAX) student.P = MAX;
+        }
+        else {
+            student.P += 20;
+
+            if (student.P > MAX) student.P = MAX;
+        }
+    }
 }
 
 void work() {
+    student.E -= 20;
+    student.P -= 10;
 
+    if (student.E >= 80) {
+        student.K += 40;
+
+        if (student.K > MAX) student.K = MAX;
+    }
+    else if (student.E <= 40) {
+        int random = randomInt(1, 2);
+
+        if (random == 1) {
+            student.K += 40;
+
+            if (student.K > MAX) student.K = MAX;
+        }
+        else {
+            student.K += 20;
+
+            if (student.K > MAX) student.K = MAX;
+        }
+    }
+    else {
+        int random = randomInt(1, 4);
+
+        if (random != 1) {
+            student.K += 40;
+
+            if (student.K > MAX) student.K = MAX;
+        }
+        else {
+            student.K += 20;
+
+            if (student.K > MAX) student.K = MAX;
+        }
+    }
 }
 
-void attendExam() {
+void attendExam(int examNumber) {
+    student.E -= 20;
+    int penalty = (examNumber - 1) * 5;
+    int luck = randomInt(1, 100);
+
+    double success = (student.Z * 0.75) + (student.P * 0.1) + (student.E * 0.1) + (luck * 0.2) - penalty;
+
+    if (success >= 75) {
+        if (student.I == 4) {
+            win();
+        }
+        else {
+            student.I++;
+            game.examNumber++;
+            student.P += 20;
+            student.E -= 20;
+
+            if (student.P > MAX) student.P = MAX;
+        }
+    }
+    else {
+        student.P -= 30;
+        student.E -= 20;
+    }
+}
+
+void checkCondition() {
 
 }
 
@@ -128,9 +412,10 @@ void loadGameLoop() {
 void newGameLoop() {
     setDifficulty();
 
-    int day = 1;
+    game.day = 1;
+    game.examNumber = 1;
 
-    while (day <= SESSION_DAYS) {
+    while (game.day <= SESSION_DAYS) {
         showStats();
 
         int isDecided = false;
@@ -173,7 +458,7 @@ void newGameLoop() {
                 isDecided = true;
                 break;
             case 6:
-                attendExam();
+                attendExam(game.examNumber);
                 isDecided = true;
                 break;
             case 11:
@@ -185,7 +470,9 @@ void newGameLoop() {
             }
         }
 
-        day++;
+        checkCondition();
+
+        game.day++;
 
         autoSave();
     }
@@ -205,15 +492,15 @@ int main()
     cin >> option;
     
     switch (option) {
-        case 1:
-            newGameLoop();
-            break;
-        case 2:
-            loadGameLoop();
-            break;
-        default:
-            cout << "Maybe if you failed entering simple number you are not ready for student city :(";
-            break;
+    case 1:
+        newGameLoop();
+        break;
+    case 2:
+        loadGameLoop();
+        break;
+    default:
+        cout << "Maybe if you failed entering simple number you are not ready for student city :(";
+        break;
     }
 
     return 0;
